c72d0204afb2310d7e35f17c8bc231252c3a63fc
jfs-coder <jfs-coder@protonmail.com>
2020-12-01 17:37:09 +0000

    changed back del to rm and added LIST_PATH const

--- todo.c
+++ todo.c
@@ -3,7 +3,8 @@
 #include <stdlib.h>
 #include <string.h>
 
-const char* VERSION = "0.2";
+const char* VERSION = "0.3";
+const char* LIST_PATH = "list";
 
 int noSwitchIsPresent(int argumentCount) {
 	// checks to see if there are arguments
@@ -26,9 +27,9 @@ int addSwitchIsPresent(int argumentCount, char* argumentString) {
 	return (argumentCount == 3) && (!strcmp("-add", argumentString));
 }
 
-int delSwitchIsPresent(int argumentCount, char* argumentString) {
+int rmSwitchIsPresent(int argumentCount, char* argumentString) {
 	// if there are three arguments and the second argument is equal to "-del" then return 1 (true)
-	return (argumentCount == 3) && (!strcmp("-del", argumentString));
+	return (argumentCount == 3) && (!strcmp("-rm", argumentString));
 }
 
 void displayHelp() {
@@ -37,13 +38,13 @@ void displayHelp() {
 	printf("todo -help (displays help)\n");
 	printf("todo -list (lists all elements)\n");
 	printf("todo -add 'something to do'\n");
-	printf("todo -del 3 (deletes 3rd element from the list)\n\n");
+	printf("todo -rm 3 (removes 3rd element from the list)\n\n");
 }
 
 void displayList() {
-	// check to see if the 'list' file exists and if so read and display elements, else print msg
+	// check to see if the '.list' file exists and if so read and display elements, else print msg
 	FILE *todoFile;
-	if( (todoFile = fopen("list", "r"))) {
+	if( (todoFile = fopen(LIST_PATH, "r"))) {
 		printf("\n[TODO LIST - Version %s]\n\n", VERSION);
 		char line[256];
 		int counter = 1;
@@ -61,7 +62,7 @@ void displayList() {
 void addElement(char* argumentString) {
 	// Open file "list" and append/write to it the entry provided
 	FILE *todoFile;
-	todoFile = fopen("list", "a");
+	todoFile = fopen(LIST_PATH, "a");
 	printf("\nAdding Entry: -> %s\n", argumentString);
 	fputs(argumentString, todoFile);
 	// here we have to add a NEWLINE manually because fputs doesn't do it automatically on our string.
@@ -69,10 +70,10 @@ void addElement(char* argumentString) {
 	fclose(todoFile);
 }
 
-void delElement(char* argumentString) {
-	int elementToRemove = atoi(argumentString); // convert arg string (choice of element to delete) to integer
+void rmElement(char* argumentString) {
+	int elementToRemove = atoi(argumentString); // convert arg string (choice of element to remove) to integer
 	FILE *todoFile;
-	if ((todoFile = fopen("list", "r"))) {
+	if ((todoFile = fopen(LIST_PATH, "r"))) {
 		char line[50][256];
 		int counter = 0; // used to iterate lines
 		int lineCounter = 0; // used to find out how many lines we are dealing with
@@ -81,12 +82,14 @@ void delElement(char* argumentString) {
 			lineCounter++;
 		}
 		fclose(todoFile);
-			todoFile = fopen("list", "w");
+
+		// write to the file, but skipping the one entry being removed.
+		todoFile = fopen(LIST_PATH, "w");
 		int i;
 		for (i = 0; i < lineCounter; i++) {
 			if(i+1 == elementToRemove) {
 				printf("\nRemoving Entry #%d -> %s", i+1, line[i]);
-				continue; // skip adding it to the file, effectively deleting it.
+				continue; // skip adding it to the file, effectively removing it.
 			}
 			fputs(line[i], todoFile);
 		}
@@ -115,8 +118,8 @@ int main(int argc, char **argv) {
 		displayList();
 	}
 
-	if (delSwitchIsPresent(argc, argv[1])) {
-		delElement(argv[2]);
+	if (rmSwitchIsPresent(argc, argv[1])) {
+		rmElement(argv[2]);
 		displayList();
 	}
 
